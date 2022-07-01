f000bf2ad288583ec3dd25196f020b64b4c75632
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-30 21:16:32 -0400

    have to figure out event listener to load items properly

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,61 @@
 // need to select div with class description
-tasks = [];
+tasks = {
+  hour9: "",
+  hour10: "",
+  hour11: "",
+  hour12: "",
+  hour13: "",
+  hour14: "",
+  hour15: "",
+  hour16: "",
+  hour17: "",
+};
 
-// need to 
+// need to load items from local storage
+var loadItems = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!tasks) {
+        tasks = {
+          hour9: "",
+          hour10: "",
+          hour11: "",
+          hour12: "",
+          hour13: "",
+          hour14: "",
+          hour15: "",
+          hour16: "",
+          hour17: ""
+        };
+    }
+
+    $.each(tasks,function () {
+        var time = $(".saveBtn").parent(".time-block").attr("id");
+        console.log(time)
+    })
+}
+
+var saveTasks = function () {
+  // save into local storage
+  tasks = localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+$(".time-block").on("click", ".saveBtn", function () {
+  // select textarea
+  var text = $("textarea").val().trim();
+
+  // find closest .time-block and retrurn id value
+  var time = $(this).parent(".time-block").attr("id");
+
+  tasks[time] = text;
+  // take textarea and save to local storage and convert into p element
+  saveTasks();
+});
 
 
 
 
-$(".saveBtn").on("click", function () {
-    var text = $("textarea").val().trim();
-    // take textarea and save to local storage and convert into p element
-    localStorage.setItem("tasks", tasks)
-})
 
+
+
+loadItems();
