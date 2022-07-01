efff5dc0398f88da7cbb4f3ae3b0efed3a957985
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-07-01 17:48:32 -0400

    all loads added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,4 +11,12 @@ $(".time-block").on("click", ".saveBtn", function () {
 
 
 $("#hour9").children(".description").val(localStorage.getItem("hour9", "text"));
+$("#hour10").children(".description").val(localStorage.getItem("hour10", "text"));
+$("#hour11").children(".description").val(localStorage.getItem("hour11", "text"));
+$("#hour12").children(".description").val(localStorage.getItem("hour12", "text"));
+$("#hour13").children(".description").val(localStorage.getItem("hour13", "text"));
+$("#hour14").children(".description").val(localStorage.getItem("hour14", "text"));
+$("#hour15").children(".description").val(localStorage.getItem("hour15", "text"));
+$("#hour16").children(".description").val(localStorage.getItem("hour16", "text"));
+$("#hour17").children(".description").val(localStorage.getItem("hour17", "text"));
 
