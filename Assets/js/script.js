6e66d2eb2db6749627a73f1c4581f67d65805b51
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-07-01 17:34:31 -0400

    restructured code not to use object to save

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,61 +1,11 @@
-// need to select div with class description
-tasks = {
-  hour9: "",
-  hour10: "",
-  hour11: "",
-  hour12: "",
-  hour13: "",
-  hour14: "",
-  hour15: "",
-  hour16: "",
-  hour17: "",
-};
-
-// need to load items from local storage
-var loadItems = function () {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-
-    if (!tasks) {
-        tasks = {
-          hour9: "",
-          hour10: "",
-          hour11: "",
-          hour12: "",
-          hour13: "",
-          hour14: "",
-          hour15: "",
-          hour16: "",
-          hour17: ""
-        };
-    }
-
-    $.each(tasks,function () {
-        var time = $(".saveBtn").parent(".time-block").attr("id");
-        console.log(time)
-    })
-}
-
-var saveTasks = function () {
-  // save into local storage
-  tasks = localStorage.setItem("tasks", JSON.stringify(tasks));
-};
-
 $(".time-block").on("click", ".saveBtn", function () {
-  // select textarea
-  var text = $("textarea").val().trim();
+  // save textarea text upon button click
+  var text = $("textarea").val().trim()
+  // save hour id upon button click
+  var hour = $(this).parent(".time-block").attr("id");
 
-  // find closest .time-block and retrurn id value
-  var time = $(this).parent(".time-block").attr("id");
+  // save data into storage 
+  localStorage.setItem(hour, text);
 
-  tasks[time] = text;
-  // take textarea and save to local storage and convert into p element
-  saveTasks();
-});
+})
 
-
-
-
-
-
-
-loadItems();
