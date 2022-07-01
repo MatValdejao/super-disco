59fe9d2e4779ada782cba3b42c2c5a2cfd44496a
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-07-01 19:36:33 -0400

    page responds to time and displays different time colors

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,31 @@
-$("#currentDay").text(moment().format("dddd" + ", " + "MMMM Do" ));
+$("#currentDay").text(moment().format("dddd" + ", " + "MMMM Do"));
+
+// check current time and compare to each
+var todayHour = parseInt(moment().format("HH"));
+
+// loop through rows and compare to todayHour variable
+var timeCompare = function () {
+  // select all container children with .time-block to loop through
+  var timeBlockEl = $(".container").children(".time-block");
+  for (var i = 0; i < timeBlockEl.length; i++) {
+    var time = timeBlockEl[i].getAttribute("id").replace("hour", "");
+    time = parseInt(time);
+
+    // compare time to current time and set background color accordingly
+    if (time > todayHour) {
+      $("#hour" + time).css("backgroundColor", "lightgreen");
+    }
+    else if (time === todayHour) {
+      $("#hour" + time).css("backgroundColor", "rgb(255, 114, 118");
+    }
+    else {
+      $("#hour" + time).css("backgroundColor", "lightgrey");
+    }
+  }
+  
+  
+
+}
 
 // upon button click, save text and hour time and display message to let user know every is saved
 $(".time-block").on("click", ".saveBtn", function () {
@@ -14,15 +41,19 @@ $(".time-block").on("click", ".saveBtn", function () {
 });
 
 var saveMessage = function () {
-  // check whether span element already exits and delete if so
-  if ($("header").children("span") != undefined) {
-    $("header").children("span").remove();
-  }
-  // create span element
-  var pMessageEl = $("<span>").addClass("saveMessage").html("Appoitment Added to<span class='red-text'> localStorage</span><i class='bi bi-check2'></i>");
+  // create span element and leave it on page for 5 seconds
+  var pMessageEl = $("<span>")
+    .addClass("saveMessage")
+    .html(
+      "Appoitment Added to<span class='red-text'> localStorage</span><i class='bi bi-check2'></i>"
+    );
 
   // append as first child to container
   $(".container").prepend(pMessageEl);
+
+  var saveInterval = setTimeout(function () {
+    $(pMessageEl).remove();
+  }, 5000) 
 }
 
 $("#hour9")
@@ -52,3 +83,5 @@ $("#hour16")
 $("#hour17")
   .children(".description")
   .val(localStorage.getItem("hour17", "text"));
+
+timeCompare();
